5cf73c38c72f50f85daaf59834848d1038d61934
Igor <Igor36lebedev@gmail.com>
2024-09-30 21:33:28 +0300

    Lesson-4 add some tests

--- service/src/main/java/org/example/entity/Payment.java
+++ service/src/main/java/org/example/entity/Payment.java
@@ -2,6 +2,8 @@ package org.example.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,7 +41,11 @@ public class Payment {
     @Column(nullable = false)
     private LocalDateTime paymentDate;
 
-    @Column(nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        TODO, IN_PROGRESS, DONE
+    }
 
 }
