79ebf6b38a67bbb4452d0f221f10c5e36e8519bc
Igor Lebedev <igor36lebedev@gmail.com>
2024-10-14 19:41:40 +0300

    refactoring tests

--- service/src/test/java/org/market/entity/CategoryHibernateTest.java
+++ service/src/test/java/org/market/entity/CategoryHibernateTest.java
@@ -1,6 +1,9 @@
 package org.market.entity;
 
 import org.junit.jupiter.api.Test;
+import org.market.dao.CategoryRepository;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,37 +11,45 @@ public class CategoryHibernateTest extends GeneralHibernateTest {
 
 
     @Test
-    void testCreateCategory() {
-        Category savedCategory = session.get(Category.class, category.getId());
+    void createCategory() {
+        CategoryRepository categoryRepository = new CategoryRepository(session);
+        Category newCategory = Category.builder()
+                .name("New child Category")
+                .description("This is a new child category")
+                .parentCategory(parentCategory)
+                .build();
+        Category savedCategory = categoryRepository.save(newCategory);
 
-        assertThat(savedCategory).isNotNull();
         assertThat(savedCategory.getId()).isNotNull();
     }
 
     @Test
-    void testReadCategory() {
-        Category foundCategory = session.get(Category.class, category.getId());
+    void readCategory() {
+        CategoryRepository categoryRepository = new CategoryRepository(session);
+        Optional<Category> foundCategory = categoryRepository.findById(category.getId());
 
-        assertThat(foundCategory).isNotNull();
-        assertThat(foundCategory).isEqualTo(category);
+        assertThat(foundCategory.get()).isNotNull();
+        assertThat(foundCategory.get()).isEqualTo(category);
     }
 
     @Test
     void updateCategory() {
-        Category foundCategory = session.get(Category.class, category.getId());
-        foundCategory.setName("Updated Category");
+        CategoryRepository categoryRepository = new CategoryRepository(session);
+        Optional<Category> foundCategory = categoryRepository.findById(category.getId());
+        foundCategory.get().setName("Updated Category");
 
-        Category updatedCategory = session.get(Category.class, category.getId());
+        Optional<Category> updatedCategory = categoryRepository.findById(category.getId());
 
-        assertThat(updatedCategory.getName()).isEqualTo("Updated Category");
+        assertThat(updatedCategory.get().getName()).isEqualTo("Updated Category");
     }
 
     @Test
     void deleteCategory() {
-        session.remove(category);
+        CategoryRepository categoryRepository = new CategoryRepository(session);
+        categoryRepository.delete(category.getId());
 
-        Category deletedCategory = session.get(Category.class, category.getId());
+        Optional<Category> deletedCategory = categoryRepository.findById(category.getId());
 
-        assertThat(deletedCategory).isNull();
+        assertThat(deletedCategory).isEmpty();
     }
 }
--- service/src/test/java/org/market/entity/GeneralHibernateTest.java
+++ service/src/test/java/org/market/entity/GeneralHibernateTest.java
@@ -1,10 +1,8 @@
 package org.market.entity;
 
-import lombok.Cleanup;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
